14b70a2992c9da0db8d4736859d375991053aaac
Sk Jameel Akhtar <76216831+SkJameelAkhtar@users.noreply.github.com>
2025-07-13 23:19:48 +0530

    Week 4 HandsOn

---
+++ Week4_Spring REST using Spring Boot 3/2. spring-rest-handson/HandsOn5_ REST - Get country based on country code/Code/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,18 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SuppressWarnings("unused")
+@SpringBootApplication
+@ImportResource("classpath:country.xml")
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+    }
+}
