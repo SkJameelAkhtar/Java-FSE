ffdd2d5e0e8a51ed6fbddf7eeec7fea036e2dafd
Sk Jameel Akhtar <76216831+SkJameelAkhtar@users.noreply.github.com>
2025-07-27 22:43:12 +0530

    Week 6 React HandsOn

---
+++ Week6_React/HandsOn_1 ReactJS-HOL/Code/react_one/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <h1>Welcome to my first React App</h1>;
+}
+
+export default App;
---
+++ Week6_React/HandsOn_2 ReactJS-HOL/Code/studentapp/src/components/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Page of Student Management Portal</h1>
+      </div>
+    );
+  }
+}
+export {About};
